10503ca559b8089d22faf4c2ce9fc2d69da3e4cf
NicolaOrecchini <nicola.orecchini@agilelab.it>
2025-12-03 16:29:49 +0100

    day 3 part 2

--- Day03/day03.py
+++ Day03/day03.py
@@ -50,4 +50,77 @@ class Part1(Day3):
 
 
 class Part2(Day3):
-    pass
+
+    def max_digit_per_string(self,
+             string: str,
+             upper_bound: int) -> str:
+        return str(
+            max(
+                int(character) for character in string
+                if int(character)<upper_bound
+            )
+        )
+
+    def has_n_more_digits_at_its_right(self,
+        bank: str,
+        position: int,
+        required_digits_at_right: int) -> bool:
+        return len(bank[position+1:]) >= required_digits_at_right
+
+    def compute_all_possible_joltages(self,
+        bank: str,
+        length: int) -> list[str] | None:
+
+        positions_of_max_digit = []
+        upper_bound = 10
+
+        if length == 1:
+            return [self.max_digit_per_string(
+                string=bank,
+                upper_bound=upper_bound,)]
+
+        while positions_of_max_digit == []:
+            max_digit = self.max_digit_per_string(
+                string=bank,
+                upper_bound=upper_bound
+            )
+
+            positions_of_max_digit = [
+                i for i, digit in enumerate(bank)
+                if digit==max_digit and
+                self.has_n_more_digits_at_its_right(
+                    bank=bank,
+                    position=i,
+                    required_digits_at_right=length-1
+                )
+            ]
+            upper_bound -= 1
+
+        all_suffixes = []
+
+        for position in positions_of_max_digit:
+            suffix_solutions = self.compute_all_possible_joltages(
+                bank=bank[position+1:],
+                length=length-1
+            )
+            all_suffixes.extend(suffix_solutions)
+
+        results = [max_digit + suffix for suffix in all_suffixes]
+
+        return results
+
+    def solve(self):
+        total_output_joltage = 0
+        for row in self.input:
+            joltages_in_row = self.compute_all_possible_joltages(
+                    bank=row,
+                    length=12
+                )
+            print(joltages_in_row)
+            max_joltage_in_row = max(
+                int(value) for value in joltages_in_row
+            )
+            total_output_joltage += max_joltage_in_row
+
+        return total_output_joltage
+
--- Day03/main.py
+++ Day03/main.py
@@ -2,9 +2,22 @@ from day03 import Day3, Part1, Part2
 
 def main():
 
-    part1 = Part1()
-    part1_result = part1.solve()
-    print("Part 1:", part1_result)
+    # part1 = Part1()
+    # part1_result = part1.solve()
+    # print("Part 1:", part1_result)
+
+    # part2 = Part2()
+    # for i in range(10):
+    #     result = part2.compute_all_possible_joltages(
+    #         bank=part2.input[i],
+    #         length=2
+    #     )
+    #     print(max(int(v) for v in result))
+
+
+    part2 = Part2()
+    result = part2.solve()
+    print(result)
 
 if __name__ == "__main__":
     main()
